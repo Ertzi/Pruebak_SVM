5db7d1bf55304103f96fc4cd7f1759d9da2408a3
Julen Ercibengoa Portatil <julenertzisa@gmail.com>
2023-10-24 23:28:35 +0200

    Add more examples

---
+++ SVM_examples.py
@@ -0,0 +1,48 @@
+import numpy as np
+from SVM_with_SGD_algorithm import svm_sgd,svm_sgd_plot, plot_X_Y
+import random
+
+random.seed = 100
+
+# Ejemplo 1:
+X1 = np.array([[-2,4,-1],[4,1,-1],[1, 6, -1],[2, 4, -1],[6, 2, -1],])
+Y1 = np.array([-1,-1,1,1,1])
+print(X1)
+print(Y1)
+w1,errors1 = svm_sgd(X1,Y1)
+svm_sgd_plot(X1,Y1,w1,errors1)
+
+# Ejemplo 2:
+X2 = np.array([[1,2,-1],[3,4,-1],[2,6,-1],[1,-5,-1],[-2,3,-1],[5,-5,-1],[-6,1,-1],[8,3,-1],[2,2,-1],[2,-3,-1],[-4,-1,-1],[0,2,-1]])
+Y2 = np.array([1,1,1,-1,1,-1,-1,1,1,-1,-1,1])
+print(X2)
+print(Y2)
+w2,errors2 = svm_sgd(X2,Y2)
+svm_sgd_plot(X2,Y2,w2,errors2)
+
+# Ejemplo 3 (Generación automática de puntos):
+n = 10# Cantidad de puntos
+dx = 15 # Distancia a la que movemos todos los puntos en el eje x
+dy = 5
+X3_1 = np.zeros((n,3))
+Y3_1 = np.zeros(n)
+for i in range(n):
+    X3_1[i] = [random.randint(-10,10)-dx, random.randint(-10,10)-dy,-1]
+    Y3_1[i] = 1
+X3_2 = np.zeros((n,3))
+Y3_2 = np.zeros(n)
+for i in range(n):
+    X3_2[i] = [random.randint(-10,10)+dx, random.randint(-10,10)+dy,-1]
+    Y3_2[i] = 0
+
+X3 = np.concatenate((X3_1,X3_2))
+Y3 = np.concatenate((Y3_1,Y3_2))
+
+print(X3)
+print(Y3)
+
+plot_X_Y(X3,Y3)
+w3,errors3 = svm_sgd(X3,Y3)
+svm_sgd_plot(X3,Y3,w3,errors3)
+
+
--- SVM_with_SGD_algorithm.py
+++ SVM_with_SGD_algorithm.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 #lets perform stochastic gradient descent to learn the seperating hyperplane between both classes
 
-def svm_sgd_plot(X, Y):
+def svm_sgd(X, Y):
     #Initialize our SVMs weight vector with zeros (3 values)
     w = np.zeros(len(X[0]))
     #The learning rate
@@ -26,8 +26,9 @@ def svm_sgd_plot(X, Y):
                 #correct classification, update our weights
                 w = w + eta * (-2  *(1/epoch)* w)
         errors.append(error)
-        
+    return w, errors
 
+def svm_sgd_plot(X,Y,w,errors):
     #lets plot the rate of classification errors during training for our SVM
     plt.plot(errors, '|')
     plt.ylim(0.5,1.5)
@@ -35,41 +36,28 @@ def svm_sgd_plot(X, Y):
     plt.xlabel('Epoch')
     plt.ylabel('Misclassified')
     plt.show()
-    
-    return w
 
-# Aplicar la función a nuestro ejemplo:
-X = np.array([
-    [-2,4,-1],
-    [4,1,-1],
-    [1, 6, -1],
-    [2, 4, -1],
-    [6, 2, -1],
-])
-y = np.array([-1,-1,1,1,1])
+    for i, sample in enumerate(X):
+        if Y[i] != 1:
+            plt.scatter(sample[0], sample[1], s=120, linewidths=2, color = "red")
+        else:
+            plt.scatter(sample[0], sample[1], s=120, linewidths=2, color =  "green")
+    # Print the hyperplane calculated by svm_sgd()
+    x2=[w[0],w[1],-w[1],w[0]]
+    x3=[w[0],w[1],w[1],-w[0]]
+    x2x3 =np.array([x2,x3])
+    X,Y,U,V = zip(*x2x3)
+    ax = plt.gca()
+    ax.quiver(X,Y,U,V,scale=1, color='blue')
 
-w = svm_sgd_plot(X,y)
-print(w)
+    plt.show()
 
-for d, sample in enumerate(X):
-    # Plot the negative samples
-    if d < 2:
-        plt.scatter(sample[0], sample[1], s=120, marker='_', linewidths=2, color = "red")
-    # Plot the positive samples
-    else:
-        plt.scatter(sample[0], sample[1], s=120, marker='+', linewidths=2, color =  "green")
+def plot_X_Y(X,Y):
+    for i, sample in enumerate(X):
+        if Y[i] != 1:
+            plt.scatter(sample[0], sample[1], s=120, linewidths=2, color = "red")
+        else:
+            plt.scatter(sample[0], sample[1], s=120, linewidths=2, color =  "green")
+    plt.show()
 
-# Add our test samples
-plt.scatter(2,2, s=120, marker='_', linewidths=2, color='blue')
-plt.scatter(4,3, s=120, marker='+', linewidths=2, color='blue')
-
-# Print the hyperplane calculated by svm_sgd()
-x2=[w[0],w[1],-w[1],w[0]]
-x3=[w[0],w[1],w[1],-w[0]]
-x2x3 =np.array([x2,x3])
-X,Y,U,V = zip(*x2x3)
-ax = plt.gca()
-ax.quiver(X,Y,U,V,scale=1, color='blue')
-
-plt.show()
 
